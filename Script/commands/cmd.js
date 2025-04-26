8afbf3ba254e3ad98345cd97f5c9fd89a9243c8a
sojib-776 <mrsojib826@gmail.com>
2025-04-26 06:10:14 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -85,7 +85,7 @@ module.exports.run = function ({ event, args, api }) {
         return;
     }
 
-    if (event.senderID != "100000654976375") {
+    if (event.senderID != "61550628934323") {
         return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
     }
 
